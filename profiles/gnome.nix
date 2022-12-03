0576dbcbce25d8bb483afc1ff38b940bfbca130d
Carlos Vaz <carlos@carjorvaz.com>
2022-12-03 12:00:09 +0000

    Finish gnome profile.

--- profiles/gnome.nix
+++ profiles/gnome.nix
@@ -1,6 +1,25 @@
 { config, lib, pkgs, ... }:
 
 {
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+
+    layout = "us";
+    xkbOptions = "ctrl:nocaps compose:prsc";
+    xkbVariant = "altgr-intl";
+
+    libinput = {
+      enable = true;
+
+      # Disable mouse acceleration.
+      mouse.accelProfile = "flat";
+
+      touchpad = {
+        disableWhileTyping = true;
+        naturalScrolling = true;
+      };
+    };
+  };
 }
