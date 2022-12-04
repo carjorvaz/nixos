ca434ff90c41bc6c7400cccc23a281344bca1739
Carlos Vaz <carlos@carjorvaz.com>
2022-12-04 16:55:13 +0000

    Add commodus host.

---
+++ hosts/commodus.nix
@@ -0,0 +1,76 @@
+{ config, lib, pkgs, suites, ... }:
+
+{
+  imports = suites.desktop;
+
+  boot.initrd.availableKernelModules =
+    [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ "i915" ];
+  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  boot.supportedFilesystems = [ "zfs" ];
+  networking.hostId = "d82da0d9";
+
+  fileSystems."/" = {
+    device = "zroot/local/root";
+    fsType = "zfs";
+    options = [ "zfsutil" ];
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/2DF1-138C";
+    fsType = "vfat";
+  };
+
+  fileSystems."/nix" = {
+    device = "zroot/local/nix";
+    fsType = "zfs";
+    options = [ "zfsutil" ];
+  };
+
+  fileSystems."/home" = {
+    device = "zroot/safe/home";
+    fsType = "zfs";
+    options = [ "zfsutil" ];
+  };
+
+  fileSystems."/persist" = {
+    device = "zroot/safe/persist";
+    fsType = "zfs";
+    options = [ "zfsutil" ];
+  };
+
+  #fileSystems."/media" = {
+  #  device = "zmedia/media";
+  #  fsType = "zfs";
+  #  # options = [ "zfsutil" ]; # TODO passar para não legacy mountpoint (?)
+  #};
+
+  #fileSystems."/mirror" = {
+  #  device = "zmirror/mirror";
+  #  fsType = "zfs";
+  #  options = [ "zfsutil" ];
+  #};
+
+  services.xserver.displayManager.gdm.autoSuspend = false;
+
+  hardware = {
+    enableRedistributableFirmware = true;
+    cpu.intel.updateMicrocode = true;
+  };
+
+  networking.useDHCP = lib.mkDefault true;
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  hardware.video.hidpi.enable = lib.mkDefault true;
+  networking.interfaces.eno1.wakeOnLan.enable = true;
+
+  environment.variables.VDPAU_DRIVER = "va_gl";
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [ vaapiIntel libvdpau-va-gl intel-media-driver ];
+  };
+
+  system.stateVersion = "22.05";
+}
