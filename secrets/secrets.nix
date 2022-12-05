28f10ee2f994f93bbd77f22a013912d8f752f9da
Carlos Vaz <carlos@carjorvaz.com>
2022-12-05 19:29:52 +0000

    Prepare secrets file.

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,12 @@
 let
-  # set ssh public keys here for your system and user
-  system = "";
-  user = "";
-  allKeys = [system user];
+  gallusUser =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMxNtlOg5VM8xN3XYfBGY3wIXrJ0vF5fBpc8s2NsLG9/ cjv@gallus";
+  users = [ gallusUser ];
+
+  gallusSystem =
+    # TODO Experimentar usar o .pub do host que já existia
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICjVjQULxTJ+NN5ekG0HLpnkyPFIAwbNCQ5EOZ4cSfCt root@gallus";
+  systems = [ gallusSystem ];
 in {
-  "secret.age".publicKeys = allKeys;
+  # "secretFile.age".publicKeys = users ++ systems;
 }
