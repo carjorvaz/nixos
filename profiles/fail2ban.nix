60977650a04214adfdca0fc1393eb69e6eb45ce1
Carlos Vaz <carlos@carjorvaz.com>
2022-12-08 10:19:18 +0000

    Create fail2ban profile.

---
+++ profiles/fail2ban.nix
@@ -0,0 +1,13 @@
+{ config, lib, pkgs, ... }:
+
+{
+    fail2ban = {
+      enable = true;
+      maxretry = 5;
+      bantime-increment = {
+        enable = true;
+        rndtime = "5min";
+        maxtime = "24h";
+      };
+    };
+}
