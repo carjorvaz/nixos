837a5aae1bca1422eebbede4652b231808201217
Carlos Vaz <carlos@carjorvaz.com>
2022-12-13 20:13:03 +0000

    Simply use the emacs-gtk package.

--- profiles/emacs/default.nix
+++ profiles/emacs/default.nix
@@ -1,17 +1,9 @@
 { config, lib, pkgs, ... }:
 
 {
-  services.emacs = let
-    myEmacs = pkgs.emacs-gtk.overrideAttrs (attrs: {
-      # I don't want emacs.desktop file because I only use
-      # emacsclient.
-      postInstall = (attrs.postInstall or "") + ''
-        rm $out/share/applications/emacs.desktop
-      '';
-    });
-  in {
+  services.emacs = {
     enable = true;
-    package = ((pkgs.emacsPackagesFor myEmacs).emacsWithPackages
+    package = ((pkgs.emacsPackagesFor pkgs.emacs-gtk).emacsWithPackages
       (epkgs: [ epkgs.vterm epkgs.pdf-tools ]));
   };
 
