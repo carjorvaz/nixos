bc19478fd872a16600d5aa6e4b9f6e26d9289046
Carlos Vaz <carlos@carjorvaz.com>
2022-12-24 14:58:19 +0000

    Cleanup acme.nix.

--- profiles/acme.nix
+++ profiles/acme.nix
@@ -7,6 +7,8 @@
     acceptTerms = true;
     defaults.email = "carlos+letsencrypt@vaz.one";
 
+    # Use services.nginx.virtualHosts."example.vaz.ovh".useACMEHost = "vaz.ovh";
+    # to use the wild card certificate on subdomains.
     certs."vaz.ovh" = {
       domain = "vaz.ovh";
       extraDomainNames = [ "*.vaz.ovh" ];
@@ -17,23 +19,4 @@
   };
 
   users.users.nginx.extraGroups = [ "acme" ];
-
-  # TODO clean-up; for testing purposes only
-  services.nginx = {
-    enable = true;
-
-    # Use recommended settings
-    recommendedGzipSettings = true;
-    recommendedOptimisation = true;
-    recommendedProxySettings = true;
-    recommendedTlsSettings = true;
-
-    virtualHosts = {
-      "test.vah.ovh" = {
-        forceSSL = true;
-        useACMEHost = "vaz.ovh";
-        root = "/var/www/test.vaz.ovh/";
-      };
-    };
-  };
 }
