ddd433b6474009d6436429cd27a867dc61f27a6c
Carlos Vaz <carlos@carjorvaz.com>
2022-12-26 11:27:06 +0000

    Refactor nginx profile.

---
+++ profiles/nginx/bastion.nix
@@ -0,0 +1,11 @@
+{ config, lib, pkgs, ... }:
+
+{
+  services.nginx.virtualHosts = {
+    "jellyfin.vaz.one" = {
+      forceSSL = true;
+      useACMEHost = "vaz.one";
+      locations."/".proxyPass = "http://100.64.0.1:8096";
+    };
+  };
+}
