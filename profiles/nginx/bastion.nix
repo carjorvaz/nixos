ef34eb9d161825c8423cc73d035102297167d57c
Carlos Vaz <carlos@carjorvaz.com>
2022-12-28 10:56:35 +0000

    Add ombi to bastion nginx.

--- profiles/nginx/bastion.nix
+++ profiles/nginx/bastion.nix
@@ -7,5 +7,11 @@
       useACMEHost = "vaz.one";
       locations."/".proxyPass = "http://100.64.0.1:8096";
     };
+
+    "ombi.vaz.one" = {
+      forceSSL = true;
+      useACMEHost = "vaz.one";
+      locations."/".proxyPass = "http://100.64.0.1:5000";
+    };
   };
 }
