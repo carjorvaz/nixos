2cc731f7c3f6f5dddc7df62e38b3714aa5ff7260
Carlos Vaz <carlos@carjorvaz.com>
2022-12-28 10:56:25 +0000

    Create ombi profile.

---
+++ profiles/ombi.nix
@@ -0,0 +1,18 @@
+{ config, lib, pkgs, ... }:
+
+let domain = "ombi.vaz.ovh";
+in {
+  services = {
+    nginx.virtualHosts.${domain} = {
+      forceSSL = true;
+      useACMEHost = "vaz.ovh";
+      locations."/".proxyPass = "http://127.0.0.1:5000";
+    };
+
+    ombi.enable = true;
+  };
+
+  users.users.ombi.extraGroups = [ "media" ];
+
+  # environment.persistence."/persist".directories = [ "/var/lib/ombi" ];
+}
