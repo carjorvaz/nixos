c874a1085b2baa29b6dd2043dc7504ad9044524e
Carlos Vaz <carlos@carjorvaz.com>
2022-12-28 10:57:05 +0000

    Create intel hardware transcoding profile.

---
+++ profiles/intel-hardware-transcoding.nix
@@ -0,0 +1,19 @@
+{ config, lib, pkgs, ... }:
+
+{
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
+
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      vaapiIntel
+      vaapiVdpau
+      libvdpau-va-gl
+      intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
+    ];
+  };
+
+}
