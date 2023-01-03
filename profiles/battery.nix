3b430962944ec43528db3fabc4b14c7594162a14
Carlos Vaz <carlos@carjorvaz.com>
2023-01-03 14:22:46 +0000

    Keep the Balanced power profile all the time.

--- profiles/battery.nix
+++ profiles/battery.nix
@@ -2,13 +2,5 @@
 
 {
   powerManagement.powertop.enable = true;
-  services = {
-    power-profiles-daemon.enable = true;
-    udev.extraRules = ''
-      SUBSYSTEM=="power_supply",ENV{POWER_SUPPLY_ONLINE}=="0",RUN+="${pkgs.power-profiles-daemon}/bin/powerprofilesctl set power-saver"
-      SUBSYSTEM=="power_supply",ENV{POWER_SUPPLY_ONLINE}=="1",RUN+="${pkgs.power-profiles-daemon}/bin/powerprofilesctl set balanced"
-    '';
-  };
-
   environment.systemPackages = with pkgs; [ powertop ];
 }
