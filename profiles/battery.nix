e6c913db0b1cbda414d36c1e778fa9c5ce632bb2
Carlos Vaz <carlos@carjorvaz.com>
2023-01-03 10:59:48 +0000

    Stop using the performance power profile.

--- profiles/battery.nix
+++ profiles/battery.nix
@@ -5,8 +5,8 @@
   services = {
     power-profiles-daemon.enable = true;
     udev.extraRules = ''
-      SUBSYSTEM=="power_supply",ENV{POWER_SUPPLY_ONLINE}=="0",RUN+="${pkgs.power-profiles-daemon}/bin/powerprofilesctl set balanced"
-      SUBSYSTEM=="power_supply",ENV{POWER_SUPPLY_ONLINE}=="1",RUN+="${pkgs.power-profiles-daemon}/bin/powerprofilesctl set performance"
+      SUBSYSTEM=="power_supply",ENV{POWER_SUPPLY_ONLINE}=="0",RUN+="${pkgs.power-profiles-daemon}/bin/powerprofilesctl set power-saver"
+      SUBSYSTEM=="power_supply",ENV{POWER_SUPPLY_ONLINE}=="1",RUN+="${pkgs.power-profiles-daemon}/bin/powerprofilesctl set balanced"
     '';
   };
 
