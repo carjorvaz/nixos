c85316ed3b1a79f77c374b50b1f345dfcf6b7c17
Carlos Vaz <carlos@carjorvaz.com>
2023-01-08 17:25:55 +0000

    Start declaratively managing GNOME using home-manager.

--- profiles/graphical/gnome.nix
+++ profiles/graphical/gnome.nix
@@ -10,4 +10,32 @@
       autoSuspend = false;
     };
   };
+
+  programs.dconf.enable = true;
+
+  home-manager.users.cjv = {
+    # Use `dconf watch /` to track stateful changes you are doing, then set them here.
+    dconf.settings = {
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+        gtk-theme = "Adwaita-dark";
+      };
+
+      "/org/gnome/desktop/peripherals/mouse" = { accel-profile = "flat"; };
+
+      "org/gnome/shell" = {
+        favorite-apps = [
+          "brave-browser.desktop"
+          "org.gnome.Console.desktop"
+          "org.gnome.Nautilus.desktop"
+          "emacs.desktop"
+          "discord.desktop"
+          "com.github.flxzt.rnote.desktop"
+          "org.gnome.Geary.desktop"
+          "Mattermost.desktop"
+          "com.nextcloud.desktopclient.nextcloud.desktop"
+        ];
+      };
+    };
+  };
 }
