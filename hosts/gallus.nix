d31a004ce046f712d2f326689a5a1105f2ac6c39
Carlos Vaz <carlos@carjorvaz.com>
2023-01-18 21:32:51 +0000

    Update gallus specific configuration.

--- hosts/gallus.nix
+++ hosts/gallus.nix
@@ -6,7 +6,7 @@
   boot.initrd.availableKernelModules =
     [ "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
   boot.kernelModules = [ "kvm-intel" "acpi_call" ];
-  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+  boot.kernelPackages = pkgs.linuxPackages_zen;
   boot.extraModulePackages = with config.boot.kernelPackages; [ acpi_call ];
 
   boot.supportedFilesystems = [ "zfs" ];
@@ -59,13 +59,87 @@
     extraPackages = with pkgs; [ vaapiIntel libvdpau-va-gl intel-media-driver ];
   };
 
+
+  powerManagement.powertop.enable = true;
+  environment.systemPackages = with pkgs; [ powertop ];
+
   services = {
-    throttled.enable = true;
     xserver.wacom.enable = true;
     udev.extraRules = ''
       # blacklist Lenovo IR camera
       SUBSYSTEM=="usb", ATTRS{idVendor}=="5986", ATTRS{idProduct}=="211a", ATTR{authorized}="0"
     '';
+
+    throttled = {
+      enable = true;
+      extraConfig = ''
+        [GENERAL]
+        # Enable or disable the script execution
+        Enabled: True
+        # SYSFS path for checking if the system is running on AC power
+        Sysfs_Power_Path: /sys/class/power_supply/AC*/online
+        # Auto reload config on changes
+        Autoreload: True
+
+        ## Settings to apply while connected to Battery power
+        [BATTERY]
+        # Update the registers every this many seconds
+        Update_Rate_s: 30
+        # Max package power for time window #1
+        PL1_Tdp_W: 29
+        # Time window #1 duration
+        PL1_Duration_s: 28
+        # Max package power for time window #2
+        PL2_Tdp_W: 44
+        # Time window #2 duration
+        PL2_Duration_S: 0.002
+        # Max allowed temperature before throttling
+        Trip_Temp_C: 85
+        # Set cTDP to normal=0, down=1 or up=2 (EXPERIMENTAL)
+        cTDP: 0
+        # Disable BDPROCHOT (EXPERIMENTAL)
+        Disable_BDPROCHOT: False
+
+        ## Settings to apply while connected to AC power
+        [AC]
+        # Update the registers every this many seconds
+        Update_Rate_s: 5
+        # Max package power for time window #1
+        PL1_Tdp_W: 44
+        # Time window #1 duration
+        PL1_Duration_s: 28
+        # Max package power for time window #2
+        PL2_Tdp_W: 44
+        # Time window #2 duration
+        PL2_Duration_S: 0.002
+        # Max allowed temperature before throttling
+        Trip_Temp_C: 95
+        # Set HWP energy performance hints to 'performance' on high load (EXPERIMENTAL)
+        # Uncomment only if you really want to use it
+        # HWP_Mode: False
+        # Set cTDP to normal=0, down=1 or up=2 (EXPERIMENTAL)
+        cTDP: 0
+        # Disable BDPROCHOT (EXPERIMENTAL)
+        Disable_BDPROCHOT: False
+
+        # All voltage values are expressed in mV and *MUST* be negative (i.e. undervolt)!
+        [UNDERVOLT]
+        # CPU core voltage offset (mV)
+        CORE: -80
+        # Integrated GPU voltage offset (mV)
+        GPU: -80
+        # CPU cache voltage offset (mV)
+        CACHE: -80
+        # System Agent voltage offset (mV)
+        UNCORE: -80
+        # Analog I/O voltage offset (mV)
+        ANALOGIO: -80
+      '';
+
+    };
+    thermald.enable = false; # throttled and thermald are mutually incompatible.
+    power-profiles-daemon.enable =
+      true; # Refrain from using the power saver profile.
   };
 
   system.stateVersion = "22.05";
