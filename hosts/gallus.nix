a99e824ff37aadb37e0d7be4d3fff980b3472724
Carlos Vaz <carlos@carjorvaz.com>
2023-01-25 15:18:48 +0000

    Switch from throttled to undervolt for undervolting.

--- hosts/gallus.nix
+++ hosts/gallus.nix
@@ -60,7 +60,6 @@
     extraPackages = with pkgs; [ vaapiIntel libvdpau-va-gl intel-media-driver ];
   };
 
-
   powerManagement.powertop.enable = true;
   environment.systemPackages = with pkgs; [ powertop ];
 
@@ -71,74 +70,15 @@
       SUBSYSTEM=="usb", ATTRS{idVendor}=="5986", ATTRS{idProduct}=="211a", ATTR{authorized}="0"
     '';
 
-    throttled = {
+    undervolt = {
       enable = true;
-      extraConfig = ''
-        [GENERAL]
-        # Enable or disable the script execution
-        Enabled: True
-        # SYSFS path for checking if the system is running on AC power
-        Sysfs_Power_Path: /sys/class/power_supply/AC*/online
-        # Auto reload config on changes
-        Autoreload: True
-
-        ## Settings to apply while connected to Battery power
-        [BATTERY]
-        # Update the registers every this many seconds
-        Update_Rate_s: 30
-        # Max package power for time window #1
-        PL1_Tdp_W: 29
-        # Time window #1 duration
-        PL1_Duration_s: 28
-        # Max package power for time window #2
-        PL2_Tdp_W: 44
-        # Time window #2 duration
-        PL2_Duration_S: 0.002
-        # Max allowed temperature before throttling
-        Trip_Temp_C: 85
-        # Set cTDP to normal=0, down=1 or up=2 (EXPERIMENTAL)
-        cTDP: 0
-        # Disable BDPROCHOT (EXPERIMENTAL)
-        Disable_BDPROCHOT: False
-
-        ## Settings to apply while connected to AC power
-        [AC]
-        # Update the registers every this many seconds
-        Update_Rate_s: 5
-        # Max package power for time window #1
-        PL1_Tdp_W: 44
-        # Time window #1 duration
-        PL1_Duration_s: 28
-        # Max package power for time window #2
-        PL2_Tdp_W: 44
-        # Time window #2 duration
-        PL2_Duration_S: 0.002
-        # Max allowed temperature before throttling
-        Trip_Temp_C: 95
-        # Set HWP energy performance hints to 'performance' on high load (EXPERIMENTAL)
-        # Uncomment only if you really want to use it
-        # HWP_Mode: False
-        # Set cTDP to normal=0, down=1 or up=2 (EXPERIMENTAL)
-        cTDP: 0
-        # Disable BDPROCHOT (EXPERIMENTAL)
-        Disable_BDPROCHOT: False
-
-        # All voltage values are expressed in mV and *MUST* be negative (i.e. undervolt)!
-        [UNDERVOLT]
-        # CPU core voltage offset (mV)
-        CORE: -80
-        # Integrated GPU voltage offset (mV)
-        GPU: -80
-        # CPU cache voltage offset (mV)
-        CACHE: -80
-        # System Agent voltage offset (mV)
-        UNCORE: -80
-        # Analog I/O voltage offset (mV)
-        ANALOGIO: -80
-      '';
-
+      coreOffset = -80;
+      gpuOffset = -80;
+      uncoreOffset = -80;
+      analogioOffset = -80;
     };
-    thermald.enable = false; # throttled and thermald are mutually incompatible.
+
+    thermald.enable = true;
     power-profiles-daemon.enable =
       true; # Refrain from using the power saver profile.
   };
