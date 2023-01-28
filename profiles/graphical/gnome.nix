f226cbbc552b981205877aac8eebbb5f70795c57
Carlos Vaz <carlos@carjorvaz.com>
2023-01-28 17:49:16 +0000

    Remove colemak configuration from gnome profile.

--- profiles/graphical/gnome.nix
+++ profiles/graphical/gnome.nix
@@ -17,7 +17,6 @@
     # Use `dconf watch /` to track stateful changes you are doing, then set them here.
     dconf.settings = {
       "org/gnome/desktop/input-sources" = {
-        sources = "[('xkb', 'us+colemak_dh'), ('xkb', 'us+altgr-intl')]";
         xkb-options = [ "lv3:ralt_switch" "compose:prsc" "ctrl:nocaps" ];
       };
 
