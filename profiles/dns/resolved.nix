f2186426b59bce5e8995b4062b68fdcb79f0b644
Carlos Vaz <carlos@carjorvaz.com>
2023-02-09 15:03:45 +0000

    systemd-resolved: return to quad9

--- profiles/dns/resolved.nix
+++ profiles/dns/resolved.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }:
 
 {
-  networking.nameservers = lib.mkDefault [ "1.1.1.1#one.one.one.one" ];
+  networking.nameservers = lib.mkDefault [ "9.9.9.9#dns.quad9.net" ];
   services.resolved = {
     enable = true;
     domains = [ "~." ];
