877f190d537a6f48119de262d414a586b377d588
Carlos Vaz <carlos@carjorvaz.com>
2023-02-18 13:17:29 +0000

    gallus: return to throttled

--- hosts/gallus.nix
+++ hosts/gallus.nix
@@ -66,17 +66,77 @@
       SUBSYSTEM=="usb", ATTRS{idVendor}=="5986", ATTRS{idProduct}=="211a", ATTR{authorized}="0"
     '';
 
-    undervolt = {
+    thermald.enable = false;
+    throttled = {
       enable = true;
-      coreOffset = -80;
-      gpuOffset = -80;
-      uncoreOffset = -80;
-      analogioOffset = -80;
+      extraConfig = ''
+        [GENERAL]
+        # Enable or disable the script execution
+        Enabled: True
+        # SYSFS path for checking if the system is running on AC power
+        Sysfs_Power_Path: /sys/class/power_supply/AC*/online
+        # Auto reload config on changes
+        Autoreload: True
+
+        ## Settings to apply while connected to Battery power
+        [BATTERY]
+        # Update the registers every this many seconds
+        Update_Rate_s: 30
+        # Max package power for time window #1
+        PL1_Tdp_W: 29
+        # Time window #1 duration
+        PL1_Duration_s: 28
+        # Max package power for time window #2
+        PL2_Tdp_W: 44
+        # Time window #2 duration
+        PL2_Duration_S: 0.002
+        # Max allowed temperature before throttling
+        Trip_Temp_C: 85
+        # Set cTDP to normal=0, down=1 or up=2 (EXPERIMENTAL)
+        cTDP: 0
+        # Disable BDPROCHOT (EXPERIMENTAL)
+        Disable_BDPROCHOT: False
+
+        ## Settings to apply while connected to AC power
+        [AC]
+        # Update the registers every this many seconds
+        Update_Rate_s: 5
+        # Max package power for time window #1
+        PL1_Tdp_W: 44
+        # Time window #1 duration
+        PL1_Duration_s: 28
+        # Max package power for time window #2
+        PL2_Tdp_W: 44
+        # Time window #2 duration
+        PL2_Duration_S: 0.002
+        # Max allowed temperature before throttling
+        Trip_Temp_C: 95
+        # Set HWP energy performance hints to 'performance' on high load (EXPERIMENTAL)
+        # Uncomment only if you really want to use it
+        # HWP_Mode: False
+        # Set cTDP to normal=0, down=1 or up=2 (EXPERIMENTAL)
+        cTDP: 0
+        # Disable BDPROCHOT (EXPERIMENTAL)
+        Disable_BDPROCHOT: False
+
+
+        [UNDERVOLT]
+        # CPU core voltage offset (mV)
+        CORE: -105
+        # Integrated GPU voltage offset (mV)
+        GPU: -85
+        # CPU cache voltage offset (mV)
+        CACHE: -105
+        # System Agent voltage offset (mV)
+        UNCORE: -85
+        # Analog I/O voltage offset (mV)
+        ANALOGIO: 0
+      '';
+
     };
 
-    thermald.enable = true;
-    power-profiles-daemon.enable =
-      true; # Refrain from using the power saver profile.
+    # Refrain from using the power saver profile.
+    power-profiles-daemon.enable = true;
   };
 
   services.wgrnl = {
