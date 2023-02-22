28b249fb67bbc9d4d451ca4094662b6f706e9788
Carlos Vaz <carlos@carjorvaz.com>
2023-02-22 21:16:49 +0000

    Return to thinkcentre

--- hosts/commodus.nix
+++ hosts/commodus.nix
@@ -6,7 +6,7 @@
   boot.initrd.availableKernelModules =
     [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ "i915" ];
-  boot.kernelPackages = pkgs.linuxPackages_xanmod;
+  boot.kernelPackages = pkgs.linuxPackages_xanmod_stable;
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
@@ -63,7 +63,7 @@
   networking = {
     useDHCP = false;
 
-    interfaces.enp2s0 = {
+    interfaces.eno1 = {
       useDHCP = false;
       wakeOnLan.enable = true;
 
