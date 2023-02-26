d8bcba797db72538d71dfecc623927caa4d93c84
Carlos Vaz <carlos@carjorvaz.com>
2023-02-26 15:07:34 +0000

    stumpwm: enable redshift

--- profiles/graphical/stumpwm.nix
+++ profiles/graphical/stumpwm.nix
@@ -2,4 +2,20 @@
 
 {
   services.xserver.displayManager.startx.enable = true;
+
+  environment.systemPackages = with pkgs; [ sbcl rlwrap ];
+
+  home-manager.users.cjv = {
+    services = {
+      redshift = {
+        enable = true;
+        latitude = 38.7;
+        longitude = -9.14;
+        temperature = {
+          day = 6500;
+          night = 2000;
+        };
+      };
+    };
+  };
 }
