c0e9fa93da5fb8a17e8cb076ff67aeb4ea2e0cd7
Carlos Vaz <carlos@carjorvaz.com>
2023-04-01 17:02:38 +0100

    stumpwm: update configuration

--- profiles/graphical/stumpwm.nix
+++ profiles/graphical/stumpwm.nix
@@ -3,18 +3,17 @@
 {
   services.xserver.displayManager.startx.enable = true;
 
-  environment.systemPackages = with pkgs; [
-    sbcl
-    rlwrap
-
-    yt-dlp
-    mpv
-    pulsemixer
-  ];
+  environment.systemPackages = with pkgs; [ sbcl rlwrap yt-dlp mpv pulsemixer ];
 
   location = {
     latitude = 38.7;
     longitude = -9.14;
+  };
+
+  fonts = {
+    enableDefaultFonts = true;
+    fonts = with pkgs; [ iosevka-comfy.comfy ];
+    fontconfig = { defaultFonts = { monospace = [ "Iosevka Comfy" ]; }; };
   };
 
   services = {
@@ -31,17 +30,31 @@
     home.file = {
       ".xinitrc".text = ''
         if test -z "$DBUS_SESSION_BUS_ADDRESS"; then
-        	eval $(dbus-launch --exit-with-session --sh-syntax)
+                 eval $(dbus-launch --exit-with-session --sh-syntax)
         fi
+
         systemctl --user import-environment DISPLAY XAUTHORITY
+        systemctl --user start graphical-session.target
 
         if command -v dbus-update-activation-environment >/dev/null 2>&1; then
                 dbus-update-activation-environment DISPLAY XAUTHORITY
         fi
 
+        xrdb -merge ~/.Xresources
         exec ~/Documents/Code/stumpwm-source/stumpwm
       '';
     };
 
+    xsession.enable = true;
+    xresources.properties = {
+      "xterm.termName" = "xterm-256color";
+      "xterm*faceName" = "Monospace";
+      "xterm*faceSize" = "14";
+      "xterm*loginshell" = true;
+      "xterm*metaSendsEscape" = true;
+      "xterm*scrollBar" = true;
+      "xterm*rightScrollBar" = true;
+      "xterm*scrollTtyOutput" = false;
+    };
   };
 }
