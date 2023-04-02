d6654552af793e1d470f2b251b2c1c5597dc8587
Carlos Vaz <carlos@carjorvaz.com>
2023-04-02 22:39:04 +0100

    commodus: go back to gnome

--- flake.nix
+++ flake.nix
@@ -133,7 +133,7 @@
               acme.dns-vaz-ovh
               dns.dnscrypt-proxy2
               homer
-              graphical.sway
+              graphical.gnome.common
               intel-hardware-transcoding
               nextcloud
               nginx.common
