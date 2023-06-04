37945d922a4e4ad6655194deae0e37b515ba561f
Carlos Vaz <carlos@carjorvaz.com>
2023-06-04 21:22:33 +0100

    update syntax

--- profiles/mail.nix
+++ profiles/mail.nix
@@ -59,7 +59,7 @@
 
         # Use Let's Encrypt certificates. Note that this needs to set up a stripped
         # down nginx and opens port 80.
-        certificateScheme = 3;
+        certificateScheme = "acme-nginx";
       };
     }
   ];
