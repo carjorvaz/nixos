15a5301d77ab24e00b75af958076b7e5f0f1bb6e
Carlos Vaz <carlos@carjorvaz.com>
2023-06-30 11:48:05 +0100

    hadrianus: create ci/cd for tobepractical.com

--- profiles/nginx/blog.nix
+++ profiles/nginx/blog.nix
@@ -25,6 +25,21 @@
       enableACME = true;
       root = "/var/www/carlosvaz.pt/";
     };
+
+    "tobepractical.com" = {
+      forceSSL = true;
+      enableACME = true;
+      root = "/var/www/tobepractical.com/";
+    };
+  };
+
+  users.users.deploy = {
+    isNormalUser = true;
+    description = "CI/CD Deploy User";
+    home = "/var/www/tobepractical.com";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG4P33Dvt0Unn15N13kwdsgPfKTsZRecXtlezYyXV65S"
+    ];
   };
 
   networking.firewall.allowedTCPPorts = [ 80 443 ];
