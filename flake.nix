017b03718380ea21e65eaf0ee90da5622d0107c3
Carlos Vaz <carlos@carjorvaz.com>
2024-04-01 21:39:49 +0100

    use unstable home manager in unstable config

--- flake.nix
+++ flake.nix
@@ -22,6 +22,9 @@
     home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
+    home-manager-unstable.url = "github:nix-community/home-manager";
+    home-manager-unstalbe.inputs.nixpkgs.follows = "nixpkgs-unstable";
+
     impermanence.url = "github:nix-community/impermanence/master";
 
     simple-nixos-mailserver.url =
@@ -134,7 +137,7 @@
             ({ config, pkgs, ... }: {
               nixpkgs.overlays = [ overlay-unstable ];
             })
-            inputs.home-manager.nixosModules.home-manager
+            inputs.home-manager-unstable.nixosModules.home-manager
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
