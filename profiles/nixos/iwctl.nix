a68173ce298c3c28be54971b5b4bf71cbbb10e42
Carlos Vaz <carlos@carjorvaz.com>
2024-04-01 21:35:08 +0100

    fix typo

--- profiles/nixos/iwctl.nix
+++ profiles/nixos/iwctl.nix
@@ -1,9 +1,9 @@
 { config, lib, pkgs, ... }:
 
 {
-  networking = {
-    # STATE: connecting to eduroam https://www.math.cmu.edu/~gautam/sj/blog/20211025-eduroam-iwd.html
-    wireless.iwd.enable = true;
+  # STATE: connecting to eduroam https://www.math.cmu.edu/~gautam/sj/blog/20211025-eduroam-iwd.html
+  networking.wireless.iwd = {
+    enable = true;
     settings.General.EnableNetworkConfiguration = true;
   };
 
