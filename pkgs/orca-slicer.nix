6980c19f22072d02b96f0b03154db763123379dc
Carlos Vaz <carlos@carjorvaz.com>
2024-04-07 11:26:32 +0100

    pkgs: update orca-slicer

--- pkgs/orca-slicer.nix
+++ pkgs/orca-slicer.nix
@@ -5,11 +5,11 @@
 # - create package from source instead of appimage (like prusa-slicer)
 appimageTools.wrapType2 {
   name = "orca-slicer";
-  src = let version = "2.0.0-beta";
+  src = let version = "2.0.0";
   in fetchurl {
     url =
       "https://github.com/SoftFever/OrcaSlicer/releases/download/v${version}/OrcaSlicer_Linux_V${version}.AppImage";
-    hash = "sha256-KsHPGlIUhaKjGv9BfAlYs4eWmFMcqclYvm2XwGRnbeA=";
+    hash = "sha256-PcCsqF1RKdSrbdp1jCF0n5Mu30EniaBEuJNw3XdPhO4=";
   };
   extraPkgs = pkgs: with pkgs; [ glib-networking webkitgtk ];
 }
