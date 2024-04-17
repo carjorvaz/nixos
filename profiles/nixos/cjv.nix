b2d0cf983b09cc27bec653126e44f811bb22493e
Carlos Vaz <carlos@carjorvaz.com>
2024-04-17 22:06:15 +0100

    ssh: update trajanus keys

--- profiles/nixos/cjv.nix
+++ profiles/nixos/cjv.nix
@@ -15,7 +15,7 @@
         lib.mkDefault config.age.secrets.cjvHashedPassword.path;
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP1OS3cOxw5+wleeTybg0sWE2z0pCj007rUO3kQHSVJ7 cjv@commodus"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID7sTv+M2QPe4bLAQTeHhAxGVVBmQes74PIXIE3o4bLl cjv@trajanus"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBK2AsWCLGKxGjkXbIUD8lIV0+48qJFNV9h7FfLLx16f cjv@trajanus"
 
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINypN31r7gUkK+bo5S3h2dvHkqgwVfis6mmvBNaOFByE cjv@mac"
         "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBACMwCJmJqjPaReJswMLojtshrhr48h8BXOvBaS+k6sP1WXjln50Twn7fNW8i5lGXpA190hIYBo5tdF/kvE3JtE= cjv@iphone"
