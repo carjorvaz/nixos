d14e7e44eb084d88aff956b9b1c471396e229fe3
Carlos Vaz <carlos@carjorvaz.com>
2024-04-22 11:22:02 +0100

    bluetooth: create profile

---
+++ profiles/nixos/bluetooth.nix
@@ -0,0 +1,18 @@
+{ config, lib, pkgs, ... }:
+
+let
+  isWindowManager =
+    if config.programs.sway.enable || config.programs.hyprland.enable then
+      true
+    else
+      false;
+in {
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+
+  services.blueman.enable = isWindowManager;
+
+  home-manager.users.cjv.services.blueman.enable = isWindowManager;
+}
