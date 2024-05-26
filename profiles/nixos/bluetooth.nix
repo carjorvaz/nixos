d57fd3af593381a616a208ce7b83659f77cf091d
Carlos Vaz <carlos@carjorvaz.com>
2024-05-26 18:29:23 +0100

    bluetooth: profiles enables blueman regardless

--- profiles/nixos/bluetooth.nix
+++ profiles/nixos/bluetooth.nix
@@ -1,18 +1,12 @@
 { config, lib, pkgs, ... }:
 
-let
-  isWindowManager =
-    if config.programs.sway.enable || config.programs.hyprland.enable then
-      true
-    else
-      false;
-in {
+{
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
   };
 
-  services.blueman.enable = isWindowManager;
+  services.blueman.enable = true;
 
-  home-manager.users.cjv.services.blueman-applet.enable = isWindowManager;
+  home-manager.users.cjv.services.blueman-applet.enable = true;
 }
