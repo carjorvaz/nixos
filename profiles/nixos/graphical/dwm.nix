702472bb984b604e217d82ae78f8ca96db918e7f
Carlos Vaz <carlos@carjorvaz.com>
2024-05-27 13:08:15 +0100

    trajanus: use dwm instead of sway

---
+++ profiles/nixos/graphical/dwm.nix
@@ -0,0 +1,104 @@
+{ config, lib, pkgs, ... }:
+
+# TODO:
+# - slock dpms patch 2 seconds timeout https://tools.suckless.org/slock/patches/dpms/
+# - remove dmenumon in dwm config
+# - remove slstatus
+
+{
+  imports = [ ./common.nix ];
+
+  nixpkgs.overlays = [
+    # TODO all very similar, abstract with a function?
+    (self: super: {
+      dmenu = super.dmenu.overrideAttrs (oldAttrs: rec {
+        patches = [ ./suckless/patches/dmenu-qalc-5.2.diff ];
+
+        configFile = super.writeText "config.h"
+          (builtins.readFile ./suckless/dmenu-5.3-config.h);
+        postPatch = ''
+          ${oldAttrs.postPatch}
+          cp ${configFile} config.h'';
+      });
+
+      dwm = super.dwm.overrideAttrs (oldAttrs: rec {
+        patches = [
+          # Move cursor to focused window/screen
+          (pkgs.fetchpatch {
+            url = "https://dwm.suckless.org/patches/warp/dwm-warp-6.4.diff";
+            sha256 = "sha256-8z41ld47/2WHNJi8JKQNw76umCtD01OUQKSr/fehfLw=";
+          })
+        ];
+
+        configFile = super.writeText "config.h"
+          (builtins.readFile ./suckless/dwm-6.5-config.h);
+        postPatch = ''
+          ${oldAttrs.postPatch}
+          cp ${configFile} config.h'';
+      });
+
+      st = super.st.overrideAttrs (oldAttrs: rec {
+        patches = [
+          (pkgs.fetchpatch {
+            url =
+              "https://st.suckless.org/patches/anysize/st-expected-anysize-0.9.diff";
+            sha256 = "sha256-q21HEZoTiVb+IIpjqYPa9idVyYlbG9RF3LD6yKW4muo=";
+          })
+        ];
+
+        configFile = super.writeText "config.h"
+          (builtins.readFile ./suckless/st-0.9.2-config.h);
+        postPatch = ''
+          ${oldAttrs.postPatch}
+          cp ${configFile} config.h'';
+      });
+
+      slstatus =
+        super.slstatus.override { conf = ./suckless/slstatus-1.0-config.h; };
+    })
+  ];
+
+  services = {
+    xserver.windowManager.dwm.enable = true;
+
+    dwm-status = {
+      enable = true;
+
+      # TODO audio?
+      order = lib.mkDefault [ "time" ];
+
+      extraConfig = ''
+        separator = " | "
+      '';
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    bemoji
+    dmenu
+    dmenu-bluetooth
+    st
+    stalonetray
+  ];
+
+  programs = {
+    slock.enable = true;
+
+    # To lock: loginctl lock-session
+    # https://discourse.nixos.org/t/slock-when-suspend/22457
+    xss-lock = {
+      enable = true;
+      lockerCommand = "/run/wrappers/bin/slock";
+    };
+  };
+
+  # TODO remove
+  # systemd.user.services.slstatus = {
+  #   description =
+  #     "status monitor service for window managers that use WM_NAME like dwm";
+  #   wantedBy = [ "graphical-session.target" ];
+  #   partOf = [ "graphical-session.target" ];
+
+  #   serviceConfig.ExecStart = "${pkgs.slstatus}/bin/slstatus";
+  # };
+}
---
+++ profiles/nixos/graphical/suckless/dwm-6.5-config.h
@@ -0,0 +1,142 @@
+/* See LICENSE file for copyright and license details. */
+#include <X11/XF86keysym.h>
+
+/* appearance */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int snap      = 32;       /* snap pixel */
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
+static const char *fonts[]          = { "monospace:size=10" };
+static const char dmenufont[]       = "monospace:size=10";
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+/* static const char col_cyan[]        = "#005577"; */
+static const char col_cyan[]        = "#444444";
+static const char *colors[][3]      = {
+	/*               fg         bg         border   */
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+};
+
+/* tagging */
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+static const Rule rules[] = {
+	/* xprop(1):
+	 *	WM_CLASS(STRING) = instance, class
+	 *	WM_NAME(STRING) = title
+	 */
+
+    /* reference: https://dwm.suckless.org/customisation/tagmask/ */
+
+	/* class      instance    title       tags mask     isfloating   monitor */
+    { "firefox",     NULL,    NULL,       1,            0,           -1 },
+    { "emacs",       NULL,    NULL,       1 << 1,       0,           -1 },
+    { "betterbird",  NULL,    NULL,       1 << 7,       0,           -1 },
+	{ "discord",     NULL,    NULL,       1 << 8,       0,           -1 },
+};
+
+/* layout(s) */
+static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const int nmaster     = 1;    /* number of clients in master area */
+static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+
+static const Layout layouts[] = {
+	/* symbol     arrange function */
+	{ "[]=",      tile },    /* first entry is default */
+	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[M]",      monocle },
+};
+
+/* key definitions */
+#define MODKEY Mod4Mask
+#define TAGKEYS(KEY,TAG) \
+	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+
+/* helper for spawning shell commands in the pre dwm-5.0 fashion */
+#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+
+/* commands */
+/* TODO remove this dmenumon so it spawns in whichever monitor is active */
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenuqalccmd[] = { "dmenu", "-C", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]  = { "st", NULL };
+static const char *scrotcmd[]  = { "flameshot", "gui", NULL };
+static const char *lockcmd[]  = { "slock", NULL };
+static const char *incbrightcmd[]  = { "light", "-T", "1.618", NULL };
+static const char *decbrightcmd[]  = { "light", "-T", "0.618", NULL };
+static const char *incvolumecmd[]  = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "+2dB", NULL };
+static const char *decvolumecmd[]  = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "-2dB", NULL };
+static const char *tgmuteoutcmd[]  = { "pamixer", "-t", NULL };
+static const char *tgmuteinpcmd[]  = { "pamixer", "--default-source", "-t", NULL };
+
+static const Key keys[] = {
+	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_c,      spawn,          {.v = dmenuqalccmd } },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY,                       XK_Tab,    view,           {0} },
+	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_space,  setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	TAGKEYS(                        XK_1,                      0)
+	TAGKEYS(                        XK_2,                      1)
+	TAGKEYS(                        XK_3,                      2)
+	TAGKEYS(                        XK_4,                      3)
+	TAGKEYS(                        XK_5,                      4)
+	TAGKEYS(                        XK_6,                      5)
+	TAGKEYS(                        XK_7,                      6)
+	TAGKEYS(                        XK_8,                      7)
+	TAGKEYS(                        XK_9,                      8)
+	/* { MODKEY|ShiftMask,             XK_q,      quit,           {0} }, */ /* commented out to avoid accidents from sway muscle memory */
+    { MODKEY|ShiftMask,             XK_p,                     spawn, {.v = scrotcmd } },
+    { MODKEY,                       XK_Escape,                spawn, {.v = lockcmd } },
+	{0,                             XF86XK_MonBrightnessUp,   spawn, {.v = incbrightcmd } },
+	{0,                             XF86XK_MonBrightnessDown, spawn, {.v = decbrightcmd } },
+	{0,                             XF86XK_AudioRaiseVolume,  spawn, {.v = incvolumecmd } },
+	{0,                             XF86XK_AudioLowerVolume,  spawn, {.v = decvolumecmd } },
+	{0,                             XF86XK_AudioMute,         spawn, {.v = tgmuteoutcmd } },
+	{0,                             XF86XK_AudioMicMute,      spawn, {.v = tgmuteinpcmd } },
+};
+
+/* button definitions */
+/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+static const Button buttons[] = {
+	/* click                event mask      button          function        argument */
+	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
+	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
+	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
+	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
+	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+	{ ClkTagBar,            0,              Button1,        view,           {0} },
+	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
+	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+};
+
