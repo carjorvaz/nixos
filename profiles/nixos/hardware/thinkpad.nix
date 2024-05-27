517d546c42cd0de9da75bcae66953a04dc7e008e
Carlos Vaz <carlos@carjorvaz.com>
2024-05-27 14:27:27 +0100

    refactor to hardware profile

---
+++ profiles/nixos/hardware/thinkpad.nix
@@ -0,0 +1,15 @@
+{ config, lib, pkgs, ... }:
+
+{
+  boot = {
+    # For battery thresholds.
+    kernelModules = [ "acpi_call" ];
+    extraModulePackages = with config.boot.kernelPackages; [ acpi_call ];
+  };
+
+  services.tlp.settings = {
+    # Extend battery longevity.
+    START_CHARGE_THRESH_BAT0 = 75;
+    STOP_CHARGE_THRESH_BAT0 = 80;
+  };
+}
