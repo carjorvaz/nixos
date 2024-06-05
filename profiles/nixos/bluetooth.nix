23f640d1bb9bcfd688e444582674bbeb29203afc
Carlos Vaz <carlos@carjorvaz.com>
2024-06-05 14:54:00 +0100

    bluetooth: persist state folder

--- profiles/nixos/bluetooth.nix
+++ profiles/nixos/bluetooth.nix
@@ -9,4 +9,6 @@
   services.blueman.enable = true;
 
   home-manager.users.cjv.services.blueman-applet.enable = true;
+
+  environment.persistence."/persist".directories = [ "/var/lib/bluetooth" ];
 }
