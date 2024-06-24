d303aea9be5df218d26f4fedd147e535f2e0933d
Carlos Vaz <carlos@carjorvaz.com>
2024-06-24 14:22:45 +0100

    tailscale: refactor

--- profiles/nixos/tailscale.nix
+++ profiles/nixos/tailscale.nix
@@ -8,12 +8,13 @@
 {
   # STATE: Run `tailscale up` on first boot.
   # Disable key expiry on the Tailscale console if necessary.
-  services.tailscale.enable = true;
-  networking.firewall = {
-    checkReversePath = "loose";
-    trustedInterfaces = [ "tailscale0" ];
-    allowedUDPPorts = [ config.services.tailscale.port ];
+  services.tailscale = {
+    enable = true;
+    openFirewall = true;
+    useRoutingFeatures = lib.mkDefault "client";
   };
+
+  networking.firewall.trustedInterfaces = [ "tailscale0" ];
 
   environment.persistence."/persist".directories = [ "/var/lib/tailscale" ];
 }
