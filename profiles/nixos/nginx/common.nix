b5cd9c3d136230bee36643a9aaadc94930769e0d
Carlos Vaz <carlos@carjorvaz.com>
2024-07-03 08:37:46 +0100

    nginx: allow for bigger request sizes

--- profiles/nixos/nginx/common.nix
+++ profiles/nixos/nginx/common.nix
@@ -8,11 +8,15 @@
 {
   services.nginx = {
     enable = true;
+
     recommendedGzipSettings = true;
     recommendedOptimisation = true;
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
     recommendedZstdSettings = true;
+
+    # For image uploads
+    clientMaxBodySize = "1G";
   };
 
   networking.firewall.allowedTCPPorts = [
