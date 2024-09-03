3075ace12e1f71fd0cee5b29a32368846dbac233
Carlos Vaz <carlos@carjorvaz.com>
2024-09-03 09:44:53 +0100

    graphical: enable quiet boot

--- profiles/nixos/graphical/common.nix
+++ profiles/nixos/graphical/common.nix
@@ -19,6 +19,29 @@ in
   # - pkgs.linuxPackages_xanmod_stable;
   boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_xanmod;
 
+  # Prettier boot
+  boot = {
+    plymouth.enable = true;
+
+    # Enable "Silent Boot"
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+
+    # Hide the OS choice for bootloaders.
+    # It's still possible to open the bootloader list by pressing any key
+    # It will just not appear on screen unless a key is pressed
+    loader.timeout = 0;
+  };
+
   # Improve desktop responsiveness when updating the system.
   nix.daemonCPUSchedPolicy = "idle";
 
