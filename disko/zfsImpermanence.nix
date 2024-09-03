bb145c2abba82321184f912f803bbebfb194151a
Carlos Vaz <carlos@carjorvaz.com>
2024-09-03 09:43:52 +0100

    disko: do zfs impermanence on shutdown
    This allows me to enable plymouth

--- disko/zfsImpermanence.nix
+++ disko/zfsImpermanence.nix
@@ -1,5 +1,17 @@
 # TODO migrate everything running with root as tmpfs to this and merge this with base.nix
-{ lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+# ZFS-based impermanence but instead of rolling back on every start, roll back on safe shutdown/halt/reboot.
+# Copied from: https://github.com/chaotic-cx/nyx/blob/d214d9f692140d2777f78e050b0757f577d14ed3/modules/nixos/zfs-impermanence-on-shutdown.nix
+
+let
+  cfgZfs = config.boot.zfs;
+in
 {
   disko.devices.zpool.zroot = {
     postCreateHook = "zfs snapshot zroot/local/root@blank";
@@ -9,14 +21,12 @@
     };
   };
 
-  boot = {
-    initrd = {
-      systemd.enable = false;
-      postDeviceCommands = lib.mkAfter ''
-        zfs rollback -r zroot/local/root@blank
-      '';
-    };
+  systemd.shutdownRamfs.contents."/etc/systemd/system-shutdown/zpool".source = lib.mkForce (
+    pkgs.writeShellScript "zpool-sync-shutdown" ''
+      ${cfgZfs.package}/bin/zfs rollback -r zroot/local/root@blank
+      exec ${cfgZfs.package}/bin/zpool sync
+    ''
+  );
 
-    plymouth.enable = false;
-  };
+  systemd.shutdownRamfs.storePaths = [ "${cfgZfs.package}/bin/zfs" ];
 }
