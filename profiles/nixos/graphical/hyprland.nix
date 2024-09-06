e1f07858cd53bb4bc1c5f2d95c8a0ce932a9914e
Carlos Vaz <carlos@carjorvaz.com>
2024-09-06 14:53:06 +0100

    hyprland: migrate from swaylock to hyprlock

--- profiles/nixos/graphical/hyprland.nix
+++ profiles/nixos/graphical/hyprland.nix
@@ -12,17 +12,15 @@
     ./wayland.nix
   ];
 
-  programs.hyprland = {
-    enable = true;
-    package = pkgs.unstable.hyprland;
-  };
-
-  # Swaylock won't work without this.
-  # TODO hyprlock?
-  security.pam.services.swaylock = { };
+  programs = {
+    hyprland = {
+      enable = true;
+      package = pkgs.unstable.hyprland;
+    };
 
   systemd.user.services = {
     gammastep.wantedBy = lib.mkForce [ "hyprland-session.target" ];
+    hyprlock.enable = true;
   };
 
   home-manager.users.cjv = {
@@ -132,7 +130,7 @@
 
         # Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
         bind = [
-          "$mainMod, ESCAPE, exec, swaylock"
+          "$mainMod, ESCAPE, exec, hyprlock"
           "$mainMod, RETURN, exec, $terminal"
           "$mainMod SHIFT, Q, hy3:killactive,"
           "$mainMod SHIFT, E, exit,"
@@ -241,6 +239,61 @@
     };
 
     programs = {
+      hyprlock = {
+        enable = true;
+        settings = {
+          background = {
+            path = "${wallpaper}";
+
+            blur_passes = 2;
+            contrast = 1;
+            brightness = 0.5;
+            vibrancy = 0.2;
+            vibrancy_darkness = 0.2;
+          };
+
+          general = {
+            no_fade_in = true;
+            no_fade_out = true;
+            hide_cursor = true;
+
+            grace = 0;
+          };
+
+          # From: https://wiki.hyprland.org/Hypr-Ecosystem/hyprlock/#input-field
+          input-field = {
+            size = "200, 50";
+            outline_thickness = 3;
+            dots_size = 0.33; # Scale of input-field height, 0.2 - 0.8
+            dots_spacing = 0.15; # Scale of dots' absolute size, 0.0 - 1.0
+            dots_center = false;
+            dots_rounding = -1; # -1 default circle, -2 follow input-field rounding
+            outer_color = "rgb(151515)";
+            inner_color = "rgb(200, 200, 200)";
+            font_color = "rgb(10, 10, 10)";
+            fade_on_empty = true;
+            fade_timeout = 1000; # Milliseconds before fade_on_empty is triggered.
+            placeholder_text = "<i>Input Password...</i>"; # Text rendered in the input box when it's empty.
+            hide_input = false;
+            rounding = -1; # -1 means complete rounding (circle/oval)
+            check_color = "rgb(204, 136, 34)";
+            fail_color = "rgb(204, 34, 34)"; # if authentication failed, changes outer_color and fail message color
+            fail_text = "<i>$FAIL <b>($ATTEMPTS)</b></i>"; # can be set to empty
+            fail_timeout = 2000; # milliseconds before fail_text and fail_color disappears
+            fail_transition = 300; # transition time in ms between normal outer_color and fail_color
+            capslock_color = -1;
+            numlock_color = -1;
+            bothlock_color = -1; # when both locks are active. -1 means don't change outer color (same for above)
+            invert_numlock = false; # change color if numlock is off
+            swap_font_color = false; # see below
+
+            position = "0, -20";
+            halign = "center";
+            valign = "center";
+          };
+        };
+      };
+
       waybar = {
         enable = true;
 
