7ff1a341daeb0fefe266ac00cde0f2958da2c56e
Carlos Vaz <carlos@carjorvaz.com>
2024-09-14 16:42:00 +0100

    hyprland: change dwindle behaviour

--- profiles/nixos/graphical/hyprland.nix
+++ profiles/nixos/graphical/hyprland.nix
@@ -94,8 +94,7 @@ in
 
         # https://wiki.hyprland.org/Configuring/Dwindle-Layout/
         dwindle = {
-          pseudotile = true; # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
-          preserve_split = true; # you probably want this
+          force_split = 2; # spawn new windows on the right/bottom
         };
 
         misc = {
