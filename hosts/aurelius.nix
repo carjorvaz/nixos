8dc3c1cba246dbf7c52bb84d7d9f8bfdd135c7d3
Carlos Vaz <carlos@carjorvaz.com>
2024-09-18 19:49:06 +0100

    aurelius: new host

---
+++ hosts/aurelius.nix
@@ -0,0 +1,180 @@
+{
+  self,
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}:
+
+let
+  networkInterface = "enp3s0";
+in
+{
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+    "${self}/profiles/nixos/base.nix"
+    "${self}/profiles/nixos/bootloader/systemd-boot.nix"
+    "${self}/profiles/nixos/cpu/amd.nix"
+    "${self}/profiles/nixos/zfs/common.nix"
+    "${self}/profiles/nixos/zramSwap.nix"
+
+    "${self}/profiles/nixos/cjv.nix"
+    "${self}/profiles/nixos/emacs.nix"
+    "${self}/profiles/nixos/graphical/hyprland.nix"
+    "${self}/profiles/nixos/docker.nix"
+    "${self}/profiles/nixos/libvirt.nix"
+    "${self}/profiles/nixos/ssh.nix"
+
+    "${self}/profiles/home/zsh.nix"
+  ];
+
+  age.secrets.aureliusInitrdHostKey.file = "${self}/secrets/aureliusInitrdHostKey.age";
+
+  boot.initrd.availableKernelModules = [
+    "xhci_pci"
+    "ahci"
+    "usb_storage"
+    "usbhid"
+    "sd_mod"
+  ];
+
+  networking = {
+    useDHCP = false;
+    hostName = "aurelius";
+    hostId = "8556b001";
+
+    networkmanager.enable = false;
+
+    domain = "rnl.tecnico.ulisboa.pt";
+    search = [ "rnl.tecnico.ulisboa.pt" ];
+    timeServers = [ "ntp.rnl.tecnico.ulisboa.pt" ];
+
+    interfaces.${networkInterface} = {
+      useDHCP = false;
+      wakeOnLan.enable = true; # Requires enabling WoL in BIOS
+
+      ipv4.addresses = [
+        {
+          address = "193.136.164.202";
+          prefixLength = 27;
+        }
+      ];
+
+      ipv6.addresses = [
+        {
+          address = "2001:690:2100:82::202";
+          prefixLength = 64;
+        }
+      ];
+    };
+
+    defaultGateway = "193.136.164.222";
+    defaultGateway6 = "2001:690:2100:82::ffff:1";
+
+    nameservers = [
+      "193.136.164.1"
+      "193.136.164.2"
+      "2001:690:2100:82::1"
+      "2001:690:2100:82::2"
+    ];
+  };
+
+  security.pki.certificateFiles =
+    let
+      RNLCert = builtins.fetchurl {
+        url = "https://rnl.tecnico.ulisboa.pt/ca/cacert/cacert.pem";
+        sha256 = "1jiqx6s86hlmpp8k2172ki6b2ayhr1hyr5g2d5vzs41rnva8bl63";
+      };
+    in
+    [ "${RNLCert}" ];
+
+  # STATE: Comment this block when deploying, as agenix won't be able to get the
+  # host keys and won't create the boot entry.
+  # After deploying, enable again and rebuild.
+  cjv.zfsRemoteUnlock = {
+    enable = true;
+    # As most firewall ports are blocked,
+    # use a more common one that still isn't 22.
+    port = 80;
+    authorizedKeys = config.users.users.cjv.openssh.authorizedKeys.keys;
+    hostKeyFile = config.age.secrets.aureliusInitrdHostKey.path;
+    driver = "r8169";
+    static = {
+      enable = true;
+      # Gets the first IP address from the system network configuration.
+      address = (builtins.head config.networking.interfaces.${networkInterface}.ipv4.addresses).address;
+      gateway = config.networking.defaultGateway.address;
+      # TODO automatically set this according to prefixLength above
+      netmask = "255.255.255.224";
+      interface = networkInterface;
+    };
+  };
+
+  users.users.cjv = {
+    isNormalUser = true;
+    description = "Carlos Vaz";
+    extraGroups = [ "wheel" ];
+    hashedPassword = "$y$j9T$tJQ1YunPkcr0P2ay/PHqP.$OLGalRqTmS8q0goP6N5jBa0YvUJrdQ0/WDoNdBYPTD5";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP1OS3cOxw5+wleeTybg0sWE2z0pCj007rUO3kQHSVJ7 cjv@commodus"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBK2AsWCLGKxGjkXbIUD8lIV0+48qJFNV9h7FfLLx16f cjv@trajanus"
+      "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEYD8N3akY3HZzv03LxEgnvctoeI6Z3MI9q4hL/m6IOE0LjXejJ3tYA56OYmRPitj73ks4I+ik7qpNHNZ6H/ktg= cjv@iphone"
+    ];
+  };
+
+  home-manager.users.cjv = {
+    home.stateVersion = "24.05";
+
+    programs.i3status-rust.bars.top.blocks = [
+      {
+        block = "sound";
+        max_vol = 100;
+        headphones_indicator = true;
+        device_kind = "sink";
+        click = [
+          {
+            button = "left";
+            cmd = "${pkgs.rofi-pulse-select}/bin/rofi-pulse-select sink";
+          }
+        ];
+      }
+      {
+        block = "time";
+        interval = 5;
+        format = " $timestamp.datetime(f:'%a %d/%m %R')";
+      }
+    ];
+
+    wayland.windowManager.sway.config = rec {
+      output = {
+        "*".bg = "~/Pictures/wallpaper.png fill";
+
+        "DP-1" = {
+          resolution = "3840x2160";
+          # Get pos from wdisplays.
+          pos = "0 86";
+          scale = "1.25";
+        };
+
+        "HDMI-A-2" = {
+          resolution = "1920x1080";
+          # Get pos from wdisplays.
+          pos = "3072 0";
+          transform = "270";
+        };
+      };
+
+      workspaceOutputAssign = [
+        {
+          workspace = "9";
+          output = "HDMI-A-2";
+        }
+      ];
+    };
+  };
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+  system.stateVersion = "24.05";
+}
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,4 +1,5 @@
 let
+  aureliusSystem = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP7f0hFBGTlQo533O73cpWP+7lQqncdYpxS2/qtYbv3A root@aurelius";
   piusSystem = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKAJul712iSthWHXLAgBh38x4lpjXgsTd2KzlP5Jnf55 root@commodus  ";
   hadrianusSystem = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFI1Mcb4pU6+2ZCmS5wBJqb4oLZdcSxryvTOUf9ZLxIU root@hadrianus";
   t440System = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBlO09UlgM2z4BKrw6GeveWdZuCX48Nzj57ujSvRYb+U root@t440";
@@ -10,6 +11,7 @@ let
     trajanusSystem
   ];
 
+  aureliusUser = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICrco+nZ1DgpsNHntTzMeo626GglxwLKks3XL82XD0kZ cjv@aurelius";
   commodusUser = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP1OS3cOxw5+wleeTybg0sWE2z0pCj007rUO3kQHSVJ7 cjv@commodus";
   trajanusUser = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBK2AsWCLGKxGjkXbIUD8lIV0+48qJFNV9h7FfLLx16f cjv@trajanus";
   users = [
@@ -18,6 +20,7 @@ let
   ];
 in
 {
+  "aureliusInitrdHostKey.age".publicKeys = [ aureliusSystem ] ++ users;
   "mailCarlosHashedPassword.age".publicKeys = [
     hadrianusSystem
     commodusUser
