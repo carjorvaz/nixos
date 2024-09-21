c3a613d0ca5beccf43ec89a1e0c754080710e1d5
Carlos Vaz <cjv@aurelius.rnl.tecnico.ulisboa.pt>
2024-09-21 19:34:37 +0100

    systemd-boot: only enable plymouth on graphical

--- profiles/nixos/bootloader/systemd-boot.nix
+++ profiles/nixos/bootloader/systemd-boot.nix
@@ -8,9 +8,6 @@
 {
   imports = [ ./common.nix ];
 
-  boot.plymouth.enable = lib.mkDefault true;
-  boot.initrd.systemd.enable = lib.mkDefault true;
-
   boot.loader.systemd-boot = {
     enable = true;
     editor = false;
