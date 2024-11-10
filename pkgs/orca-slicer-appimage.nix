1162bdcf862e0391d68246e002b7dda0380d6464
Carlos Vaz <carlos@carjorvaz.com>
2024-11-10 15:22:22 +0000

    orca-slicer: upgrade to 2.2.0

--- pkgs/orca-slicer-appimage.nix
+++ pkgs/orca-slicer-appimage.nix
@@ -6,16 +6,16 @@ appimageTools.wrapType2 {
   name = "orca-slicer-appimage";
   src =
     let
-      version = "2.2.0-beta";
+      version = "2.2.0";
     in
     fetchurl {
-      url = "https://github.com/SoftFever/OrcaSlicer/releases/download/v${version}/OrcaSlicer_Linux_Ubuntu2404_V${version}.AppImage";
-      hash = "sha256-jTRfcs84MDbiM6G/sYM9eV8tEGINa1irPGmuFfj+tTM=";
+      url = "https://github.com/SoftFever/OrcaSlicer/releases/download/v${version}/OrcaSlicer_Linux_V${version}.AppImage";
+      hash = "sha256-3uqA3PXTrrOE0l8ziRAtmQ07gBFB+1Zx3S6JhmOPrZ8=";
     };
 
   extraPkgs =
     pkgs: with pkgs; [
       glib-networking
-      webkitgtk_4_1
+      webkitgtk_4_0
     ];
 }
