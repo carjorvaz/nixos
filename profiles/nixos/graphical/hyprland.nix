c80e4e9d8c7cdc2f803c099355f8a117acd8605b
Carlos Vaz <carlos@carjorvaz.com>
2024-11-14 20:58:38 +0000

    hyprlock: change style and timeout

--- profiles/nixos/graphical/hyprland.nix
+++ profiles/nixos/graphical/hyprland.nix
@@ -267,56 +267,65 @@ in
     programs = {
       hyprlock = {
         enable = true;
+        # https://github.com/MrVivekRajan/Hyprlock-Styles/blob/main/Style-9/hyprlock.conf
         settings = {
           background = {
             path = "${wallpaper}";
 
             blur_passes = 2;
-            contrast = 1;
-            brightness = 0.5;
-            vibrancy = 0.2;
-            vibrancy_darkness = 0.2;
+            contrast = 0.8916;
+            brightness = 0.8172;
+            vibrancy = 0.1696;
+            vibrancy_darkness = 0.0;
           };
 
           general = {
-            no_fade_in = true;
-            no_fade_out = true;
-            hide_cursor = true;
-
+            no_fade_in = false;
             grace = 0;
+            disable_loading_bar = false;
           };
 
-          # From: https://wiki.hyprland.org/Hypr-Ecosystem/hyprlock/#input-field
           input-field = {
-            size = "200, 50";
-            outline_thickness = 3;
-            dots_size = 0.33; # Scale of input-field height, 0.2 - 0.8
-            dots_spacing = 0.15; # Scale of dots' absolute size, 0.0 - 1.0
-            dots_center = false;
-            dots_rounding = -1; # -1 default circle, -2 follow input-field rounding
-            outer_color = "rgb(151515)";
-            inner_color = "rgb(200, 200, 200)";
-            font_color = "rgb(10, 10, 10)";
-            fade_on_empty = true;
-            fade_timeout = 1000; # Milliseconds before fade_on_empty is triggered.
-            placeholder_text = "<i>Input Password...</i>"; # Text rendered in the input box when it's empty.
+            size = "250, 60";
+            outline_thickness = 2;
+            dots_size = 0.2; # Scale of input-field height, 0.2 - 0.8
+            dots_spacing = 0.2; # Scale of dots' absolute size, 0.0 - 1.0
+            dots_center = true;
+            outer_color = "rgba(0, 0, 0, 0)";
+            inner_color = "rgba(100, 114, 125, 0.4)";
+            font_color = "rgb(200, 200, 200)";
+            fade_on_empty = false;
+            font_family = "JetBrainsMono Nerd Font Bold";
+            placeholder_text = ''<i><span foreground="##ffffff99">Enter Password</span></i>'';
             hide_input = false;
-            rounding = -1; # -1 means complete rounding (circle/oval)
-            check_color = "rgb(204, 136, 34)";
-            fail_color = "rgb(204, 34, 34)"; # if authentication failed, changes outer_color and fail message color
-            fail_text = "<i>$FAIL <b>($ATTEMPTS)</b></i>"; # can be set to empty
-            fail_timeout = 2000; # milliseconds before fail_text and fail_color disappears
-            fail_transition = 300; # transition time in ms between normal outer_color and fail_color
-            capslock_color = -1;
-            numlock_color = -1;
-            bothlock_color = -1; # when both locks are active. -1 means don't change outer color (same for above)
-            invert_numlock = false; # change color if numlock is off
-            swap_font_color = false; # see below
-
-            position = "0, -20";
+            position = "0, -225";
             halign = "center";
             valign = "center";
           };
+
+          label = [
+            # Time
+            {
+              text = ''cmd[update:1000] echo "<span>$(date +"%H:%M")</span>"'';
+              color = "rgba(216, 222, 233, 0.70)";
+              font_size = 130;
+              font_family = "JetBrainsMono Nerd Font Bold";
+              position = "0, 240";
+              halign = "center";
+              valign = "center";
+            }
+
+            # Day-Month-Date
+            {
+              text = ''cmd[update:1000] echo -e "$(date +"%A, %d %B")"'';
+              color = "rgba(216, 222, 233, 0.70)";
+              font_size = 30;
+              font_family = "JetBrainsMono Nerd Font Bold";
+              position = "0, 105";
+              halign = "center";
+              valign = "center";
+            }
+          ];
         };
       };
 
@@ -611,7 +620,7 @@ in
           listener = [
             {
               # Turn off screen after two seconds of inactivity when locked
-              timeout = 2;
+              timeout = 5;
               on-timeout = "${pkgs.procps}/bin/pidof hyprlock && ${pkgs.hyprland}/bin/hyprctl dispatch dpms off";
               on-resume = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
             }
