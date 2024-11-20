0178dec51a9897cbd56c6c8c1079acf896023507
Carlos Vaz <carlos@carjorvaz.com>
2024-11-20 08:56:02 +0000

    hy3: disable autotiling

--- profiles/nixos/graphical/hyprland.nix
+++ profiles/nixos/graphical/hyprland.nix
@@ -243,12 +243,6 @@ in
             text_font = "monospace";
             text_height = 10;
           };
-
-          autotile = {
-            enable = true;
-            trigger_width = 800;
-            trigger_height = 500;
-          };
         };
       };
 
