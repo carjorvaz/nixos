1ee0df3f20fb7dc076f6d1dac824713a8eaf0a75
Carlos Vaz <carlos.vaz@rnl.tecnico.ulisboa.pt>
2024-12-05 15:50:28 +0000

    orca-slicer-appimage: update package definition

--- pkgs/orca-slicer-appimage.nix
+++ pkgs/orca-slicer-appimage.nix
@@ -2,16 +2,17 @@
 
 # TODO/maybe:
 # - use makeDesktopItem to create a proper desktop entry
+let
+  version = "2.2.0";
+in
 appimageTools.wrapType2 {
-  name = "orca-slicer-appimage";
-  src =
-    let
-      version = "2.2.0";
-    in
-    fetchurl {
-      url = "https://github.com/SoftFever/OrcaSlicer/releases/download/v${version}/OrcaSlicer_Linux_V${version}.AppImage";
-      hash = "sha256-3uqA3PXTrrOE0l8ziRAtmQ07gBFB+1Zx3S6JhmOPrZ8=";
-    };
+  inherit version;
+  pname = "orca-slicer-appimage";
+
+  src = fetchurl {
+    url = "https://github.com/SoftFever/OrcaSlicer/releases/download/v${version}/OrcaSlicer_Linux_V${version}.AppImage";
+    hash = "sha256-3uqA3PXTrrOE0l8ziRAtmQ07gBFB+1Zx3S6JhmOPrZ8=";
+  };
 
   extraPkgs =
     pkgs: with pkgs; [
