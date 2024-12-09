1dab9126f8411a6c17e0b5dc4d268963f99c09c8
Carlos Vaz <carlos@carjorvaz.com>
2024-12-09 21:19:15 +0000

    sonarr: temp fix for 24.11

--- profiles/nixos/sonarr.nix
+++ profiles/nixos/sonarr.nix
@@ -22,5 +22,14 @@ in
     };
   };
 
+  # TODO: remove when fixed
+  # https://github.com/NixOS/nixpkgs/issues/360592#issuecomment-2513490613
+  nixpkgs.config.permittedInsecurePackages = [
+    "aspnetcore-runtime-6.0.36"
+    "aspnetcore-runtime-wrapped-6.0.36"
+    "dotnet-sdk-6.0.428"
+    "dotnet-sdk-wrapped-6.0.428"
+  ];
+
   environment.persistence."/persist".directories = [ "/var/lib/sonarr" ];
 }
