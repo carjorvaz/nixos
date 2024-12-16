27eb301b2cdac25639a6577cb155f200543e04de
Carlos Vaz <carlos@carjorvaz.com>
2024-12-16 21:25:19 +0000

    firefox: update sidebery userchrome

--- profiles/nixos/graphical/common.nix
+++ profiles/nixos/graphical/common.nix
@@ -398,18 +398,35 @@ in
               preface = "[Sidebery] "; # Enable and set same preface in Sidebery settings
             in
             ''
-              #main-window #titlebar {
+              /**
+               * Dynamic Horizontal Tabs Toolbar (with animations)
+               * sidebar.verticalTabs: false (with native horizontal tabs)
+               */
+              #main-window #TabsToolbar > .toolbar-items {
                 overflow: hidden;
                 transition: height 0.3s 0.3s !important;
               }
               /* Default state: Set initial height to enable animation */
-              #main-window #titlebar { height: 3em !important; }
-              #main-window[uidensity="touch"] #titlebar { height: 3.35em !important; }
-              #main-window[uidensity="compact"] #titlebar { height: 2.7em !important; }
+              #main-window #TabsToolbar > .toolbar-items { height: 3em !important; }
+              #main-window[uidensity="touch"] #TabsToolbar > .toolbar-items { height: 3.35em !important; }
+              #main-window[uidensity="compact"] #TabsToolbar > .toolbar-items { height: 2.7em !important; }
               /* Hidden state: Hide native tabs strip */
-              #main-window[titlepreface*="${preface}"] #titlebar { height: 0 !important; }
+              #main-window[titlepreface*="${preface}"] #TabsToolbar > .toolbar-items { height: 0 !important; }
               /* Hidden state: Fix z-index of active pinned tabs */
               #main-window[titlepreface*="${preface}"] #tabbrowser-tabs { z-index: 0 !important; }
+              /* Hidden state: Hide window buttons in tabs-toolbar */
+              #main-window[titlepreface*="${preface}"] #TabsToolbar .titlebar-spacer,
+              #main-window[titlepreface*="${preface}"] #TabsToolbar .titlebar-buttonbox-container {
+                display: none !important;
+              }
+              /* [Optional] Uncomment block below to show window buttons in nav-bar (maybe, I didn't test it on non-linux-i3wm env) */
+              /* #main-window[titlepreface*="${preface}"] #nav-bar > .titlebar-buttonbox-container,
+              #main-window[titlepreface*="${preface}"] #nav-bar > .titlebar-buttonbox-container > .titlebar-buttonbox {
+                display: flex !important;
+              } */
+              /* [Optional] Uncomment one of the line below if you need space near window buttons */
+              /* #main-window[titlepreface*="${preface}"] #nav-bar > .titlebar-spacer[type="pre-tabs"] { display: flex !important; } */
+              /* #main-window[titlepreface*="${preface}"] #nav-bar > .titlebar-spacer[type="post-tabs"] { display: flex !important; } */
             '';
         };
       };
