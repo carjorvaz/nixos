8c102a18a59ec07a890b57ee9c08d152cb3e1398
Carlos Vaz <carlos@carjorvaz.com>
2024-12-16 21:19:52 +0000

    gnome: revamp gnome config, inspired by omakub

--- profiles/nixos/graphical/gnome.nix
+++ profiles/nixos/graphical/gnome.nix
@@ -1,15 +1,13 @@
 {
-  config,
-  lib,
   pkgs,
   ...
 }:
 
 {
-  # TODO faster "xrate"
-  imports = [ ./common.nix ];
+  imports = [ ./wayland.nix ];
 
   environment.systemPackages = with pkgs; [
+    alacritty
     celluloid
     drawing
     foliate
@@ -19,10 +17,20 @@
     inkscape
     metadata-cleaner
     pdfslicer
+    ptyxis
     qalculate-gtk
     waypipe
     wl-clipboard
+
+    gnomeExtensions.appindicator
+    gnomeExtensions.just-perfection
+    gnomeExtensions.blur-my-shell
+    gnomeExtensions.space-bar
+    gnomeExtensions.undecorate
   ];
+
+  # https://wiki.nixos.org/wiki/GNOME#Systray_Icons
+  services.udev.packages = [ pkgs.gnome-settings-daemon ];
 
   services.xserver = {
     enable = true;
@@ -98,15 +106,65 @@
           two-finger-scrolling-enabled = true;
         };
 
-        # Enable fractional scaling.
+        "org/gnome/desktop/wm/preferences" = {
+          focus-mode = "mouse";
+          num-workspaces = 9;
+        };
+
+        "org/gnome/shell/keybindings" = lib.pipe (lib.range 1 9) [
+          (lib.map toString)
+          (lib.map (i: [
+            (lib.nameValuePair "switch-to-application-${i}" (
+              lib.hm.gvariant.mkEmptyArray lib.hm.gvariant.type.string
+            ))
+          ]))
+          lib.flatten
+          builtins.listToAttrs
+        ];
+
+        "org/gnome/desktop/peripherals/keyboard" = {
+          delay = lib.hm.gvariant.mkUint32 300;
+          repeat-interval = lib.hm.gvariant.mkUint32 30;
+        };
+
+        "org/gnome/desktop/wm/keybindings" =
+          lib.pipe (lib.range 1 9) [
+            (lib.map toString)
+            (lib.map (i: [
+              (lib.nameValuePair "switch-to-workspace-${i}" [ "<Super>${i}" ])
+              (lib.nameValuePair "move-to-workspace-${i}" [ "<Shift><Super>${i}" ])
+            ]))
+            lib.flatten
+            builtins.listToAttrs
+          ]
+          // {
+            close = [ "<Shift><Super>q" ];
+          };
+
         "org/gnome/mutter" = {
+          # Enable fractional scaling.
           experimental-features = [ "scale-monitor-framebuffer" ];
+
+          center-new-windows = true;
+          current-workspace-only = true;
+          dynamic-workspaces = false;
+          workspaces-only-on-primary = true;
         };
 
         "org/gnome/settings-daemon/plugins/color" = {
           night-light-enabled = true;
-          night-light-temperature = lib.hm.gvariant.mkUint32 1700;
+          night-light-temperature = lib.hm.gvariant.mkUint32 1200;
           night-light-schedule-automatic = true;
+        };
+
+        "org/gnome/settings-daemon/plugins/media-keys" = {
+          screensaver = [ "<Super>o" ];
+        };
+
+        # STATE: Needs to be created manually
+        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+          binding = "<Super>Return";
+          command = "alacritty";
         };
 
         "org/gnome/settings-daemon/plugins/power" = {
@@ -120,14 +178,53 @@
         "org/gnome/shell" = {
           favorite-apps = [
             "brave-browser.desktop"
-            "org.gnome.Console.desktop"
+            "Alacritty.desktop"
             "org.gnome.Nautilus.desktop"
             "emacs.desktop"
-            "org.gnome.Geary.desktop"
+            "thunderbird.desktop"
             "Mattermost.desktop"
             "com.nextcloud.desktopclient.nextcloud.desktop"
           ];
+
+          # https://wiki.nixos.org/wiki/GNOME#Managing_extensions
+          disable-user-extensions = false; # enables user extensions
+          enabled-extensions = [
+            pkgs.gnomeExtensions.appindicator.extensionUuid
+            pkgs.gnomeExtensions.just-perfection.extensionUuid
+            pkgs.gnomeExtensions.blur-my-shell.extensionUuid
+            pkgs.gnomeExtensions.space-bar.extensionUuid
+            pkgs.gnomeExtensions.undecorate.extensionUuid
+          ];
         };
+
+        # Configure Just Perfection
+        "org/gnome/shell/extensions/just-perfection" = {
+          animation = 2;
+          dash-app-running = true;
+          workspace = true;
+          workspace-popup = false;
+        };
+
+        # Configure Blur My Shell
+        "org/gnome/shell/extensions/blur-my-shell/appfolder".blur = false;
+        "org/gnome/shell/extensions/blur-my-shell/lockscreen".blur = false;
+        "org/gnome/shell/extensions/blur-my-shell/screenshot".blur = false;
+        "org/gnome/shell/extensions/blur-my-shell/window-list".blur = false;
+        "org/gnome/shell/extensions/blur-my-shell/panel".blur = false;
+        "org/gnome/shell/extensions/blur-my-shell/overview".blur = true;
+        "org/gnome/shell/extensions/blur-my-shell/overview".pipeline = "pipeline_default";
+        "org/gnome/shell/extensions/blur-my-shell/dash-to-dock".blur = true;
+        "org/gnome/shell/extensions/blur-my-shell/dash-to-dock".brightness = "0/6";
+        "org/gnome/shell/extensions/blur-my-shell/dash-to-dock".sigma = 30;
+        "org/gnome/shell/extensions/blur-my-shell/dash-to-dock".static-blur = true;
+        "org/gnome/shell/extensions/blur-my-shell/dash-to-dock".style-dash-to-dock = 0;
+
+        # Configure Space Bar
+        "org/gnome/shell/extensions/space-bar/behavior".smart-workspace-names = false;
+        "org/gnome/shell/extensions/space-bar/shortcuts".enable-activate-workspace-shortcuts = false;
+        "org/gnome/shell/extensions/space-bar/shortcuts".enable-move-to-workspace-shortcuts = true;
+        "org/gnome/shell/extensions/space-bar/shortcuts".open-menu =
+          lib.hm.gvariant.mkEmptyArray lib.hm.gvariant.type.string;
       };
     };
 }
