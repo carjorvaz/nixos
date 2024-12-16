0249debc6fd7bc388cc328c04a853312b1745cdc
Carlos Vaz <carlos@carjorvaz.com>
2024-12-16 19:00:46 +0000

    gnome: enable dynamic triple buffering

--- profiles/nixos/graphical/gnome.nix
+++ profiles/nixos/graphical/gnome.nix
@@ -33,6 +33,37 @@
     };
   };
 
+  # https://discourse.nixos.org/t/overlays-seem-ignored-when-sudo-nixos-rebuild-switch-gnome-47-triple-buffering-compilation-errors/55434/12
+  nixpkgs.overlays = [
+    (final: prev: {
+      mutter = prev.mutter.overrideAttrs (oldAttrs: {
+        # GNOME dynamic triple buffering (huge performance improvement)
+        # See https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/1441
+        src = final.fetchFromGitLab {
+          domain = "gitlab.gnome.org";
+          owner = "vanvugt";
+          repo = "mutter";
+          rev = "triple-buffering-v4-47";
+          hash = "sha256-JaqJvbuIAFDKJ3y/8j/7hZ+/Eqru+Mm1d3EvjfmCcug=";
+        };
+
+        preConfigure =
+          let
+            gvdb = final.fetchFromGitLab {
+              domain = "gitlab.gnome.org";
+              owner = "GNOME";
+              repo = "gvdb";
+              rev = "2b42fc75f09dbe1cd1057580b5782b08f2dcb400";
+              hash = "sha256-CIdEwRbtxWCwgTb5HYHrixXi+G+qeE1APRaUeka3NWk=";
+            };
+          in
+          ''
+            cp -a "${gvdb}" ./subprojects/gvdb
+          '';
+      });
+    })
+  ];
+
   environment.sessionVariables = {
     # Make electron apps run on Wayland natively.
     NIXOS_OZONE_WL = "1";
