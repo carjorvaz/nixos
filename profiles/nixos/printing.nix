467e86dd8423e1c9022087f26fbbfdb6d47d870a
Carlos Vaz <carlos@carjorvaz.com>
2024-12-27 21:11:24 +0000

    printing: remove canon driver as it won't build

--- profiles/nixos/printing.nix
+++ profiles/nixos/printing.nix
@@ -9,7 +9,7 @@
   services.printing = {
     enable = true;
     drivers = [
-      pkgs.cups-bjnp
+      # pkgs.cups-bjnp # Broken as of 2024/12/77 https://github.com/NixOS/nixpkgs/issues/368624
       pkgs.gutenprint
       pkgs.gutenprintBin
     ];
