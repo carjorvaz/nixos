2c6bcc4547edad9b8c54706a65b24084837a7314
Carlos Vaz <carlos@carjorvaz.com>
2025-01-16 09:11:45 +0000

    mac: fix flake

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
 
-    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-24.11";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs-darwin";
 
     determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/0.1";
