7c2b9a2cf60455308a084d4e714c8b0f9c36b0e2
Carlos Vaz <carlos@carjorvaz.com>
2025-01-16 09:11:56 +0000

    mac: install brainworkshop dependencies

--- hosts/mac.nix
+++ hosts/mac.nix
@@ -75,6 +75,20 @@
     texlab
     texlive.combined.scheme-full # Quite big, around 20GB. Remove if I'm running out of space.
 
+    # brainworkshop dependencies
+    (
+      # https://github.com/NixOS/nixpkgs/issues/368061
+      (python3Packages.pyglet.override {
+        glibc = "<dummy>";
+      }).overridePythonAttrs
+        (old: {
+          postPatch = builtins.replaceStrings [ "<dummy>/lib/libc.dylib.6" ] [ "/usr/lib/libc.dylib" ] (
+            old.postPatch or ""
+          );
+        })
+    )
+    ffmpeg
+
     fzf
     hugo
     go
