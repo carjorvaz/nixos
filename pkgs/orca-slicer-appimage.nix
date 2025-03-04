689aa129199f9777bbc5588e83959df51c80957a
Carlos Vaz <carlos@carjorvaz.com>
2025-03-04 09:56:56 +0000

    orca-slicer: bump version

--- pkgs/orca-slicer-appimage.nix
+++ pkgs/orca-slicer-appimage.nix
@@ -2,14 +2,14 @@
 # TODO/maybe:
 # - use makeDesktopItem to create a proper desktop entry
 let
-  version = "2.2.0";
+  version = "2.3.0-beta2";
 in
 appimageTools.wrapType2 {
   inherit version;
   pname = "orca-slicer-appimage";
   src = fetchurl {
-    url = "https://github.com/SoftFever/OrcaSlicer/releases/download/v${version}/OrcaSlicer_Linux_V${version}.AppImage";
-    hash = "sha256-3uqA3PXTrrOE0l8ziRAtmQ07gBFB+1Zx3S6JhmOPrZ8=";
+    url = "https://github.com/SoftFever/OrcaSlicer/releases/download/v${version}/OrcaSlicer_Linux_AppImage_V${version}.AppImage";
+    hash = "sha256-c0Lryyv9uhdbpESI3FDv7rBNP7eGpZXicormzYf2u5I=";
   };
   extraPkgs =
     pkgs: with pkgs; [
