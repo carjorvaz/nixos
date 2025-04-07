aab2869680bca5229430f408b9e47bfa7a2e8d8e
Carlos Vaz <carlos@carjorvaz.com>
2025-04-07 11:29:08 +0100

    pius: set up home assistant

---
+++ profiles/nixos/home-assistant.nix
@@ -0,0 +1,60 @@
+{ config, ... }:
+
+let
+  domain = "home-assistant.vaz.ovh";
+in
+{
+  services = {
+    nginx.virtualHosts.${domain} = {
+      forceSSL = true;
+      useACMEHost = "vaz.ovh";
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:${toString config.services.home-assistant.config.http.server_port}";
+        proxyWebsockets = true;
+      };
+    };
+
+    home-assistant = {
+      enable = true;
+
+      # To find out components:
+      # - run `❯ journalctl -efu home-assistant`
+      # - find component in https://github.com/NixOS/nixpkgs/blob/master/pkgs/servers/home-assistant/component-packages.nix
+      extraComponents = [
+        # Components required to complete the onboarding
+        "analytics"
+        "google_translate"
+        "met"
+        "radio_browser"
+        "shopping_list"
+        # Recommended for fast zlib compression
+        # https://www.home-assistant.io/integrations/isal
+        "isal"
+
+        "esphome"
+      ];
+
+      config = {
+        # Includes dependencies for a basic setup
+        # https://www.home-assistant.io/integrations/default_config/
+        default_config = { };
+
+        http = {
+          server_port = 8124;
+          trusted_proxies = [
+            "127.0.0.1"
+          ];
+          use_x_forwarded_for = true;
+        };
+      };
+    };
+  };
+
+  environment.persistence."/persist".directories = [
+    {
+      directory = "/var/lib/hass";
+      user = "hass";
+      group = "hass";
+    }
+  ];
+}
