031d7489d2735a03004cdedfa2f673ba6511bbae
Carlos Vaz <carlos@carjorvaz.com>
2025-05-27 21:45:28 +0100

    update to nixos 25.05

--- profiles/nixos/locale.nix
+++ profiles/nixos/locale.nix
@@ -1,26 +1,27 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{ pkgs, ... }:
 
 {
   time.timeZone = "Europe/Lisbon";
 
   i18n = {
-    defaultLocale = "en_US.utf8";
+    defaultLocale = "en_US.UTF-8";
+
     extraLocaleSettings = {
-      LC_ADDRESS = "pt_PT.utf8";
-      LC_IDENTIFICATION = "pt_PT.utf8";
-      LC_MEASUREMENT = "pt_PT.utf8";
-      LC_MONETARY = "pt_PT.utf8";
-      LC_NAME = "pt_PT.utf8";
-      LC_NUMERIC = "pt_PT.utf8";
-      LC_PAPER = "pt_PT.utf8";
-      LC_TELEPHONE = "pt_PT.utf8";
-      LC_TIME = "pt_PT.utf8";
+      LC_ADDRESS = "pt_PT.UTF-8";
+      LC_IDENTIFICATION = "pt_PT.UTF-8";
+      LC_MEASUREMENT = "pt_PT.UTF-8";
+      LC_MONETARY = "pt_PT.UTF-8";
+      LC_NAME = "pt_PT.UTF-8";
+      LC_NUMERIC = "pt_PT.UTF-8";
+      LC_PAPER = "pt_PT.UTF-8";
+      LC_TELEPHONE = "pt_PT.UTF-8";
+      LC_TIME = "pt_PT.UTF-8";
     };
+
+    supportedLocales = [
+      "en_US.UTF-8/UTF-8"
+      "pt_PT.UTF-8/UTF-8"
+    ];
   };
 
   environment.systemPackages = with pkgs; [
