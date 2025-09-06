6e78ee222d0cc872791b23dab2207996657eb787
Carlos Vaz <carlos@carjorvaz.com>
2025-09-06 22:53:59 +0100

    zramSwap: improve tuning

--- profiles/nixos/zramSwap.nix
+++ profiles/nixos/zramSwap.nix
@@ -2,4 +2,12 @@
 
 {
   zramSwap.enable = true;
+
+  # https://github.com/NixOS/nixpkgs/pull/351002/files
+  boot.kernel.sysctl = {
+    "vm.swappiness" = 150;
+    "vm.watermark_boost_factor" = 0;
+    "vm.watermark_scale_factor" = 125;
+    "vm.page-cluster" = 0;
+  };
 }
