fe52425ce1b304cb3b8d04293581b40b7c2156fe
Carlos Vaz <carlos@carjorvaz.com>
2025-09-07 09:47:56 +0100

    add trajanus ssh key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,7 +2,7 @@ let
   piusSystem = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKAJul712iSthWHXLAgBh38x4lpjXgsTd2KzlP5Jnf55 root@commodus  ";
   hadrianusSystem = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFI1Mcb4pU6+2ZCmS5wBJqb4oLZdcSxryvTOUf9ZLxIU root@hadrianus";
   juliusSystem = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINEIQuRyifNhgbFI8ufu22kcj1Jx8WkTRlpl2HIFGZBZ root@julius";
-  trajanusSystem = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAbiiJr+X+25mBGrcKj+2i8ESORUYAv/FpeS+7LCb+nj root@trajanus";
+  trajanusSystem = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPGoGQXDEcTd0T72g+YRzoQO30E09BvbfD9eBtcl3NRf root@trajanus";
   systems = [
     piusSystem
     hadrianusSystem
@@ -11,7 +11,7 @@ let
   ];
 
   macUser = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKabE30sEDKJPK6Oq5zHn80qyakDSMqG3Y5tAfcUs2c9 cjv@mac";
-  trajanusUser = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBK2AsWCLGKxGjkXbIUD8lIV0+48qJFNV9h7FfLLx16f cjv@trajanus";
+  trajanusUser = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF+Oo/MZBaS2tWj8/QGYFnYSnXT8i9AaTE4dJs4TRSUr cjv@trajanus";
   users = [
     macUser
     trajanusUser
@@ -20,21 +20,27 @@ in
 {
   "mailCarlosHashedPassword.age".publicKeys = [
     hadrianusSystem
-  ] ++ users;
+  ]
+  ++ users;
   "mailMafaldaHashedPassword.age".publicKeys = [
     hadrianusSystem
-  ] ++ users;
+  ]
+  ++ users;
   "mailPiusPassword.age".publicKeys = [
     piusSystem
-  ] ++ users;
+  ]
+  ++ users;
   "mailPiusHashedPassword.age".publicKeys = [
     hadrianusSystem
-  ] ++ users;
+  ]
+  ++ users;
   "nextcloud-admin-pass.age".publicKeys = [
     piusSystem
-  ] ++ users;
+  ]
+  ++ users;
   "ovh.age".publicKeys = systems ++ users;
   "plausibleSecretKeybase.age".publicKeys = [
     piusSystem
-  ] ++ users;
+  ]
+  ++ users;
 }
