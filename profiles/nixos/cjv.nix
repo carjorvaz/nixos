ede573f4e680e71c7e4fa84a2153406a7ca01930
Carlos Vaz <carlos@carjorvaz.com>
2025-09-07 09:49:01 +0100

    ssh: update iphone key

--- profiles/nixos/cjv.nix
+++ profiles/nixos/cjv.nix
@@ -19,7 +19,7 @@
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP1OS3cOxw5+wleeTybg0sWE2z0pCj007rUO3kQHSVJ7 cjv@commodus"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF+Oo/MZBaS2tWj8/QGYFnYSnXT8i9AaTE4dJs4TRSUr cjv@trajanus"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKabE30sEDKJPK6Oq5zHn80qyakDSMqG3Y5tAfcUs2c9 cjv@mac"
-        "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEYD8N3akY3HZzv03LxEgnvctoeI6Z3MI9q4hL/m6IOE0LjXejJ3tYA56OYmRPitj73ks4I+ik7qpNHNZ6H/ktg= cjv@iphone"
+        "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBKF7XfdIXX9DslNHkLj6TPOLI1QaK3a4G0q1dj1fTDE70vzhWkxTPNKQjiRGGAq5SnrNlwwWPeu9xnX1GECZRfo= cjv@iphone"
       ];
     };
   };
