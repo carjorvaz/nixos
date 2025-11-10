f10fd7ee523b0c65ab0d2824393fb0815862154f
Carlos Vaz <carlos@carjorvaz.com>
2025-11-10 17:02:22 +0000

    iwd: use as networkmanager backend

--- profiles/nixos/iwd.nix
+++ profiles/nixos/iwd.nix
@@ -1,17 +1,24 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   # STATE: connecting to eduroam:
   # - https://www.math.cmu.edu/~gautam/sj/blog/20211025-eduroam-iwd.html
   # - https://wiki.nixos.org/wiki/Iwd#Eduroam_(WPA2_Enterprise)_network
-  networking.wireless.iwd = {
-    enable = true;
+  networking = {
+    networkmanager = {
+      enable = true;
+      wifi.backend = "iwd";
+    };
 
-    # Conflicts with dhcpcd
-    settings.General.EnableNetworkConfiguration = true;
+    wireless.iwd = {
+      enable = true;
+
+      settings.General = {
+        AddressRandomization = "network";
+        AddressRandomizationRange = "nic";
+      };
+    };
   };
-
-  networking.useDHCP = false;
 
   environment.persistence."/persist".directories = [ "/var/lib/iwd" ];
 }
