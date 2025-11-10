82c1ad917a3c0bfb54eb28cc23e3cc85f1d9b300
Carlos Vaz <carlos@carjorvaz.com>
2025-11-10 17:04:24 +0000

    update waybar

--- profiles/nixos/graphical/waybar/common.nix
+++ profiles/nixos/graphical/waybar/common.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   home-manager.users.cjv.programs.waybar = {
@@ -10,8 +10,10 @@
       spacing = 6;
       layer = "top";
       position = "top";
+
       modules-right = [
         "tray"
+        "network"
         "pulseaudio"
         "backlight"
         "battery"
@@ -31,33 +33,55 @@
           "󰃟"
           "󰃠"
         ];
+
         on-scroll-down = "${pkgs.light}/bin/light -T 0.618";
         on-scroll-up = "${pkgs.light}/bin/light -T 1.618";
+
+        tooltip = false;
       };
 
       battery = {
         states = {
           warning = 25;
-          critical = 15;
+          critical = 10;
         };
 
         format = "{icon} {capacity}%";
-        format-charging = "󰂉 {capacity}%";
         format-plugged = "󰚥 {capacity}%";
         format-alt = "{icon} {time}";
 
-        format-icons = [
-          "󰁺"
-          "󰁻"
-          "󰁼"
-          "󰁽"
-          "󰁾"
-          "󰁿"
-          "󰂀"
-          "󰂁"
-          "󰂂"
-          "󰁹"
-        ];
+        format-icons = {
+          charging = [
+            "󰢜"
+            "󰂆"
+            "󰂇"
+            "󰂈"
+            "󰢝"
+            "󰂉"
+            "󰢞"
+            "󰂊"
+            "󰂋"
+            "󰂅"
+          ];
+
+          default = [
+            "󰁺"
+            "󰁻"
+            "󰁼"
+            "󰁽"
+            "󰁾"
+            "󰁿"
+            "󰂀"
+            "󰂁"
+            "󰂂"
+            "󰁹"
+          ];
+        };
+
+        tooltip-format-discharging = "{power:>1.2f}W↓ {capacity}%";
+        tooltip-format-charging = "{power:>1.2f}W↑ {capacity}%";
+
+        interval = 5;
       };
 
       clock = {
@@ -68,11 +92,27 @@
       };
 
       network = {
-        format-wifi = "󰖩 {essid}";
-        format-ethernet = "{ipaddr}/{cidr} 󰈀";
-        format-linked = "{ifname} (No IP)";
-        format-disconnected = "Disconnected";
+        format = "{icon}";
         format-alt = "{ifname}: {ipaddr}/{cidr}";
+        format-wifi = "{icon}  {essid}";
+        format-ethernet = "󰈀 {ipaddr}/{cidr}";
+        format-disconnected = "󰤮 Disconnected";
+
+        format-icons = [
+          "󰤯"
+          "󰤟"
+          "󰤢"
+          "󰤥"
+          "󰤨"
+        ];
+
+        tooltip-format-wifi = "{essid} ({frequency} GHz)\n⇣{bandwidthDownBytes}  ⇡{bandwidthUpBytes}";
+        tooltip-format-ethernet = "⇣{bandwidthDownBytes}  ⇡{bandwidthUpBytes}";
+
+        on-click-right = "${pkgs.foot}/bin/foot ${pkgs.networkmanager}/bin/nmtui";
+
+        interval = 3;
+        spacing = 1;
       };
 
       pulseaudio = {
@@ -90,11 +130,12 @@
         on-click-right = "${pkgs.pamixer}/bin/pamixer --default-source -t";
         on-scroll-down = "${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ -2dB";
         on-scroll-up = "${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ +2dB";
+
         tooltip = false;
       };
     };
 
-    style = ''
+    style = lib.mkOrder 200 ''
       @keyframes blink-critical {
           0% {
               color: rgb(237, 237, 237);
@@ -117,6 +158,7 @@
       }
       }
 
+      /* TODO move to separate files */
       /* COLORS */
       /*
       @define-color bg #000000;
@@ -147,22 +189,14 @@
       @define-color critical #ed4a46;
       */
 
-      /* Gruvbox Dark colors */
-      /*
-      @define-color bg #282828;
-      @define-color fg #ebdbb2;
-      @define-color green_accent #98971a;
-      @define-color blue_accent #458588;
-      @define-color warning #d79921;
-      @define-color critical #cc241d;
-      */
-
       /* Modus Operandi light theme colors */
+      /*
       @define-color bg #ffffff;
       @define-color fg #000000;
       @define-color accent #0058a3;
       @define-color warning #a45e00;
       @define-color critical #d03c3c;
+      */
 
       /* Reset all styles */
       * {
