8bc389e2da9f00594d5156f9c9a292a79c8c0b78
Carlos Vaz <carlos@carjorvaz.com>
2025-11-30 21:37:47 +0000

    hadrianus: enable zfs remote unlock

--- hosts/hadrianus.nix
+++ hosts/hadrianus.nix
@@ -78,28 +78,37 @@ in
     };
   };
 
-  # TODO
-  # # STATE: Comment this block when deploying, as agenix won't be able to get the
-  # # host keys and won't create the boot entry.
-  # # After deploying, enable again and rebuild.
-  # cjv.zfsRemoteUnlock = {
-  #   enable = true;
-  #   # Prevent the MitM message by using different host keys in the same host.
-  #   port = 2222;
-  #   authorizedKeys = config.users.users.root.openssh.authorizedKeys.keys;
-  #   hostKeyFile = config.age.secrets.aureliusInitrdHostKey.path; # TODO how to generate key instructions in module
-  #   driver = "virtio_pci";
-  #   static = {
-  #     enable = true;
-  #     # Gets the first IP address from the system network configuration.
-  #     address = (builtins.head
-  #       config.networking.interfaces.${networkInterface}.ipv4.addresses).address;
-  #     gateway = config.networking.defaultGateway.address;
-  #     # TODO automatically set this according to prefixLength above
-  #     netmask = "255.255.252.0";
-  #     interface = networkInterface;
-  #   };
-  # };
+  # https://nixos.wiki/wiki/Agenix#Using_secrets_in_initrd
+  age.secrets.hadrianusInitrdHostKey = {
+    file = "${self}/secrets/hadrianusInitrdHostKey.age";
+    path = "/etc/initrd-hostkey";
+    symlink = false;
+  };
+
+  # STATE: Comment this block when deploying, as agenix won't be able to get the
+  # host keys and won't create the boot entry.
+  # After deploying, enable again and rebuild.
+  # Same steps if adding a new secret: add secret, rebuild with commented block, then rebuild properly with block.
+  cjv.zfsRemoteUnlock = {
+    enable = true;
+    # Prevent the MitM message by using different host keys in the same host.
+    port = 2222;
+    authorizedKeys = config.users.users.root.openssh.authorizedKeys.keys;
+
+    # https://nixos.wiki/wiki/Agenix#Using_secrets_in_initrd
+    hostKeyFile = "/etc/initrd-hostkey";
+
+    driver = "virtio_pci";
+    static = {
+      enable = true;
+      # Gets the first IP address from the system network configuration.
+      address = (builtins.head config.networking.interfaces.${networkInterface}.ipv4.addresses).address;
+      gateway = config.networking.defaultGateway.address;
+      # TODO automatically set this according to prefixLength above
+      netmask = "255.255.252.0";
+      interface = networkInterface;
+    };
+  };
 
   services.tailscale.useRoutingFeatures = "both";
 
